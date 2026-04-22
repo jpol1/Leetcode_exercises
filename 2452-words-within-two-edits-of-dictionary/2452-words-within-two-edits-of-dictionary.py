8991faa24705000a6501b1343f4a68c4964188ff
Jakub <146631834+jpol1@users.noreply.github.com>
2026-04-22 16:11:26 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -13,8 +13,9 @@ class Solution(object):
                 for letter in range(len_w):
                     if word[letter] != dct[letter]:
                         counter += 1
-                if counter <= 2:
-                    res.append(queries[idx])
-                    break
+                        if counter > 2:
+                            break
+                res.append(queries[idx])
+                break
         return res
         
