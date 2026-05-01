ea2554e019345d4662d94ec59f030956540f4f56
Jakub <146631834+jpol1@users.noreply.github.com>
2026-05-01 14:03:26 +0200

    Time: 8 ms (46.57%), Space: 11 MB (73.23%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int32_t size = static_cast<int32_t>(arr.size());
-        int32_t result = 0, cond_a, cond_b, cond_c;
-        for (int32_t i = 0; i < size; i++) {
-            for (int32_t j = i+1; j < size; j++) {
-                for(int32_t k = j+1; k < size; k) {
+        int size = static_cast<int>(arr.size());
+        int result = 0, cond_a, cond_b, cond_c;
+        for (int i = 0; i < size; i++) {
+            for (int j = i+1; j < size; j++) {
+                for(int k = j+1; k < size; k++) {
                     cond_a = abs(arr[i] - arr[j]);
                     cond_b = abs(arr[j] - arr[k]);
                     cond_c = abs(arr[i] - arr[k]);
