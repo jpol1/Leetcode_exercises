0b2437d4762aceec73ff54b2f8da5034def13d70
Jakub <146631834+jpol1@users.noreply.github.com>
2026-05-01 13:50:20 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        int a = n, b = 2, r, nwd;
-        while (b != 0) {
-            r = a % b;
-            a = b;
-            b = r;
+        if (n%2 == 0) {
+            return n;
         }
-            
-
-        return (n*2)/a;
+        else {
+            return (n*2);
+        }
     }
 };
