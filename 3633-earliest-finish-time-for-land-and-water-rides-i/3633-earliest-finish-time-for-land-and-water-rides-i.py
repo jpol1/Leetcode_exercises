4f2e86ec4cb1ddca075eb1ae6d9ecd1b0fa83fa4
Jakub <146631834+jpol1@users.noreply.github.com>
2026-06-02 10:28:48 +0200

    Time: 12 ms (59.3%), Space: 19.4 MB (56.98%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,25 +1,18 @@
-from math import factorial
-
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        min_res = float("inf")
-        for land_idx in range(len(landStartTime)):
-            for water_idx in range(len(waterStartTime)):
-                land_end = landStartTime[land_idx] + landDuration[land_idx]
-                water_end = waterStartTime[water_idx] + waterDuration[water_idx]
-                
-                if land_end >= waterStartTime[water_idx]:
-                    min_res = min(land_end + waterDuration[water_idx], min_res)
-                else:
-                    diff = waterStartTime[water_idx] - land_end
-                    min_res = min(land_end + diff + waterDuration[water_idx], min_res)
+        def find_minimum(start1, duration1, start2, duration2):
+            finish1 = float("inf")
+            for idx in range(len(start1)):
+                finish1 = min(finish1, start1[idx] + duration1[idx])
+            finish2 = float("inf")
 
-                if water_end >= landStartTime[land_idx]:
-                    min_res = min(water_end + landDuration[land_idx], min_res)
-                else:
-                    diff = landStartTime[land_idx] - water_end
-                    min_res = min(water_end + diff + landDuration[land_idx], min_res)
+            for idx in range(len(start2)):
+                finish2 = min(finish2, max(finish1, start2[idx]) + duration2[idx])
+            
+            return finish2
+        
+        land_first = find_minimum(landStartTime, landDuration, waterStartTime, waterDuration)
 
-        return min_res
+        water_first = find_minimum(waterStartTime, waterDuration, landStartTime, landDuration)
 
-
+        return min(land_first, water_first)
