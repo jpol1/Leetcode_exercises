3f22b5f7b46a868a0291cac9bb420a73e265ca67
Jakub <146631834+jpol1@users.noreply.github.com>
2026-06-03 20:50:21 +0200

    Time: 224 ms (97.1%), Space: 21.6 MB (72.93%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,42 @@
+def dfs(arr: List[List[int]], idx: tuple):
+    #O(n*m) bo może przejść po całej tablicy. ALE przechodzi max raz po każdym bo po dodaniu go na stos
+    #zmienia wartość pod jego indeksem na 0.
+    n = len(arr)
+    m = len(arr[0])
+    stack = [idx]
+    
+    arr[idx[0]][idx[1]] = "0"
+    
+    while stack:
+        row, col = stack.pop()
+        if (row + 1 < n and arr[row+1][col] == "1"):
+            stack.append( (row+1, col) )
+            arr[row+1][col] = "0"
+        
+        if (row - 1 >= 0 and arr[row-1][col] == "1"):
+            stack.append( (row-1, col) )
+            arr[row-1][col] = "0"
+            
+        if (col + 1 < m and arr[row][col+1] == "1"):
+            stack.append( (row, col+1) )
+            arr[row][col+1] = "0"
+        
+        if (col - 1 >= 0 and arr[row][col-1] == "1"):
+            stack.append( (row, col-1) )
+            arr[row][col-1] = "0"
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row, col = 0, 0
+        counter = 0
+        n = len(grid)
+        m = len(grid[0])
+        
+        #Theta(n*m)
+        for row in range(n):
+            for col in range(m):
+                if grid[row][col] == "1":
+                    dfs(grid, (row, col) ) #O(n*m)
+                    counter += 1
+            
+        return counter
